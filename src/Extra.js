a22accf64c71db7ead008b477025a8b5bf6f3a22
ovi <ataurovishek@gmail.com>
2024-10-04 16:07:11 +0600

    few changes for more optimistic performances

---
+++ src/Extra.js
@@ -0,0 +1,29 @@
+const items = [
+    {
+        id: 1,
+        title: "React Commerce",
+        img: 'https://images.pexels.com/photos/7620577/pexels-photo-7620577.jpeg?auto=compress&cs=tinysrgb&w=600',
+        desc: "Morning control cloud fairly captain stronger variety feet excitment matter sent lost hurried closely learn wonder planning poetry ten life national there orbit regular.Morning control cloud fairly captain stronger variety feet excitment matter sent"
+    },
+    {
+        id: 2,
+        title: "Next.js Blog",
+        img: 'https://images.pexels.com/photos/4549410/pexels-photo-4549410.jpeg?auto=compress&cs=tinysrgb&w=600',
+        desc: "Lorem ipsum dolor sit amet  corrupti, hic veniam consectetur adipisicing elit. Dolorem corrupti, hic veniam  consectetur adipisicing elit. Dolorem corrupti, hic veniam consequatur quidem dignissimos delectus in odio sequi, obcaecati, rerum et quibusdam vitae autem?"
+    },
+    {
+        id: 3,
+        title: "Vanilla Js App",
+        img: 'https://images.pexels.com/photos/5483075/pexels-photo-5483075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        desc: "Lorem ipsum dolor sit amet  corrupti, hic veniam consectetur adipisicing elit. Dolorem corrupti, hic veniam  consectetur adipisicing elit. Dolorem corrupti, hic veniam consequatur quidem dignissimos delectus in odio sequi, obcaecati, rerum et quibusdam vitae autem?"
+    },
+    {
+        id: 4,
+        title: "Music App",
+        img: 'https://images.pexels.com/photos/6686455/pexels-photo-6686455.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        desc: "Lorem ipsum dolor sit amet  corrupti, hic veniam consectetur adipisicing elit. Dolorem corrupti, hic veniam  consectetur adipisicing elit. Dolorem corrupti, hic veniam consequatur quidem dignissimos delectus in odio sequi, obcaecati, rerum et quibusdam vitae autem?"
+    },
+]
+
+
+export default items
